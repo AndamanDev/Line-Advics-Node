36058b7bedc8dc67410f5ea1d27b5ae176238466
andamandev02 <132320640+andamandev02@users.noreply.github.com>
2023-11-21 11:27:05 +0700

    //day 1 ครั้งที่ 2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request');
+const axios = require('axios'); 
 const app = express();
 const PORT = process.env.PORT || 4000;
 
