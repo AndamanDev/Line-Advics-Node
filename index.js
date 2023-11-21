7ce6ab1054bdbbcdeed0812cfa1b7da361908398
andamandev02 <132320640+andamandev02@users.noreply.github.com>
2023-11-21 15:00:49 +0700

    //day 1

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ const port = process.env.PORT || 4000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Handle GET request at the root path
+app.get('/', (req, res) => {
+    res.send('OK');
+});
+
 app.post('/webhook', (req, res) => {
-    // let reply_token = req.body.events[0].replyToken;
-    // reply(reply_token);
-    // res.sendStatus(200);
-    res.send('Webhook is working!');
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
 });
 
 app.listen(port, () => {
@@ -22,21 +26,18 @@ function reply(reply_token) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer 37vAy1tI5xYwMV/B5dqYYrj/GQOeAMOPMSdvI7x8gFT+K0QI4hpUbM00Am7QbpkPGOMytOLoA3XfPuc4cUGeQXmqhnsKErRJIzohOAFVI36067hsjRpWwWPYtq014lJZGjKrLCIy48Zf8FBoJfoftAdB04t89/1O/w1cDnyilFU='
-    };
-
+    }
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
-                type: 'text',
-                text: 'Hello'
-            },
-            {
-                type: 'text',
-                text: 'How are you?'
-            }
-        ]
-    });
-
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
     console.log('Requesting Line API...');
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
