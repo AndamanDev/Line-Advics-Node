3569c2f80f5c3a86c7b709f60481c082fc752219
andamandev02 <132320640+andamandev02@users.noreply.github.com>
2023-11-22 11:19:51 +0700

    //day 2 test4

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const axios = require('axios').default
 const dotenv = require('dotenv')
 const env = dotenv.config().parsed
 const app = express()
+const PORT = process.env.PORT || 4000;
 
 const lineConfig = {
     channelAccessToken: '37vAy1tI5xYwMV/B5dqYYrj/GQOeAMOPMSdvI7x8gFT+K0QI4hpUbM00Am7QbpkPGOMytOLoA3XfPuc4cUGeQXmqhnsKErRJIzohOAFVI36067hsjRpWwWPYtq014lJZGjKrLCIy48Zf8FBoJfoftAdB04t89/1O/w1cDnyilFU=',
@@ -24,27 +25,27 @@ app.post('/webhook', line.middleware(lineConfig), async (req , res) => {
 
 const handleEvent = async (event) => {
     console.log(event);
-    if (event.type === 'message' && (event.message.text === 'พนักงาน Advisc' || event.message.text === 'ผู้มาติดต่อ')) {
-        const { data } = await axios.get('https://${env.RAPID_URL}/words/${event.message.text}/synonyms' , {
-            headers: { 
-                'x-rapiddapi-host': env.RAPID_URLM ,
-                'x-rapiddapi-key': env.RAPID_KEY
-            } 
-        })
-        const { synonyms } = data
-        let str = ''
+    // if (event.type === 'message' && (event.message.text === 'พนักงาน Advisc' || event.message.text === 'ผู้มาติดต่อ')) {
+    //     const { data } = await axios.get('https://${env.RAPID_URL}/words/${event.message.text}/synonyms' , {
+    //         headers: { 
+    //             'x-rapiddapi-host': env.RAPID_URLM ,
+    //             'x-rapiddapi-key': env.RAPID_KEY
+    //         } 
+    //     })
+    //     const { synonyms } = data
+    //     let str = ''
 
-        if (event.message.text === 'พนักงาน Advisc') {
-            return client.replyMessage(event.replyToken, { type: 'text', text: 'กำลัลงลิงก์ไปยัง หน้า Advics' });
-        } else if (event.message.text === 'ผู้มาติดต่อ') {
-            return client.replyMessage(event.replyToken, { type: 'text', text: 'กำลังส่งไปยังลิงก์ปลายทาง' });
-        }
-    } else {
+    //     if (event.message.text === 'พนักงาน Advisc') {
+    //         return client.replyMessage(event.replyToken, { type: 'text', text: 'กำลัลงลิงก์ไปยัง หน้า Advics' });
+    //     } else if (event.message.text === 'ผู้มาติดต่อ') {
+    //         return client.replyMessage(event.replyToken, { type: 'text', text: 'กำลังส่งไปยังลิงก์ปลายทาง' });
+    //     }
+    // } else {
         return client.replyMessage(event.replyToken, { type: 'text', text: 'กรุณารอเจ้าหน้าที่ตอบกลับ' });
-    }
+    // }
 }
 
-app.listen(4000 , () => {
-    console.log('listening on 4000');
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
 });
 
