daf5e41b977a84b8bbd4b2cb7713e0913c2f64b0
andamandev02 <132320640+andamandev02@users.noreply.github.com>
2024-01-04 20:03:58 +0700

    //fix

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ const handleEvent = async (event) => {
             action: {
               type: "uri",
               label: "นัดหมาย",
-              uri: "https://example.com/appointment",
+              uri: "https://advicsvms.andamandev.com/v1/apt/check-to-apt?user_line_id=${userId}",
             },
           },
         ],
