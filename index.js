2b0b5ba2d2fb56cece6cfc174f502159715e4a96
andamandev02 <132320640+andamandev02@users.noreply.github.com>
2024-01-04 19:20:22 +0700

    //fix

--- index.js
+++ index.js
@@ -27,28 +27,15 @@ app.post("/webhook", line.middleware(lineConfig), async (req, res) => {
 });
 
 const handleEvent = async (event) => {
-  console.log(event);
-  // if (event.type === 'message' && (event.message.text === 'พนักงาน Advisc' || event.message.text === 'ผู้มาติดต่อ')) {
-  //     const { data } = await axios.get('https://${env.RAPID_URL}/words/${event.message.text}/synonyms' , {
-  //         headers: {
-  //             'x-rapiddapi-host': env.RAPID_URLM ,
-  //             'x-rapiddapi-key': env.RAPID_KEY
-  //         }
-  //     })
-  //     const { synonyms } = data
-  //     let str = ''
-
-  //     if (event.message.text === 'พนักงาน Advisc') {
-  //         return client.replyMessage(event.replyToken, { type: 'text', text: 'กำลัลงลิงก์ไปยัง หน้า Advics' });
-  //     } else if (event.message.text === 'ผู้มาติดต่อ') {
-  //         return client.replyMessage(event.replyToken, { type: 'text', text: 'กำลังส่งไปยังลิงก์ปลายทาง' });
-  //     }
-  // } else {
+  //   return client.replyMessage(event.replyToken, {
+  //     type: "text",
+  //     text: "กรุณารอเจ้าหน้าที่ตอบกลับ",
+  //   });
+  const userId = event.source.userId;
   return client.replyMessage(event.replyToken, {
     type: "text",
-    text: "กรุณารอเจ้าหน้าที่ตอบกลับ",
+    text: `Line UID ของคุณคือ: ${userId}`,
   });
-  // }
 };
 
 app.listen(PORT, () => {
